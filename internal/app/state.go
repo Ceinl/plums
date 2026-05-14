f59d34e1e0c20c51f6b65691bd35ce4fd51268c6
Ceinl <Slyva.dima@gmail.com>
2026-05-14 18:07:06 +0200

    app package changes, replaced TextBox with Editor and positions of pannels on split view

--- internal/app/state.go
+++ internal/app/state.go
@@ -20,7 +20,7 @@ type State struct {
 	height int
 
 	messages []Message
-	TextBox  *components.TextBox
+	Editor   *components.Editor
 
 	aioutput    string
 	isStreaming bool
@@ -31,22 +31,18 @@ type State struct {
 }
 
 func NewState(width int, height int) *State {
-	tb := components.NewTextBox()
-	tb.SetMultiline(false)
 	return &State{
-		width:   width,
-		height:  height,
-		TextBox: tb,
+		width:  width,
+		height: height,
+		Editor: components.NewTextEditor(),
 	}
 }
 
 func (s *State) SubmitInput() string {
-	input := s.TextBox.GetContent()
+	input := s.Editor.GetContent()
 	if input != "" {
 		s.messages = append(s.messages, Message{Role: "user", Content: input})
-		s.TextBox.SetContent("")
-		s.TextBox.SetCursor(0)
-		s.TextBox.ClearSelection()
+		s.Editor.SetContent("")
 	}
 	return input
 }
@@ -84,15 +80,11 @@ func (s *State) SwitchLayout() {
 	switch s.Layout {
 	case LayoutDefault:
 		s.Layout = LayoutSplit
-		s.TextBox.SetMultiline(true)
 	case LayoutSplit:
 		s.Layout = LayoutFullscreen
-		s.TextBox.SetMultiline(true)
 	case LayoutFullscreen:
 		s.Layout = LayoutDefault
-		s.TextBox.SetMultiline(false)
 	default:
 		s.Layout = LayoutDefault
-		s.TextBox.SetMultiline(false)
 	}
 }
